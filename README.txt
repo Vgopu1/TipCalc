88f97e0e2bb7cd70302f3908e13065c8947c0a97
Roy <roysolvesproblems@gmail.com>
2015-10-03 02:30:14 -0700

    Initial commit.

---
+++ README.txt
@@ -0,0 +1,29 @@
+# Swift Tip Calculator
+Because sometimes a TODO list is too hard as a first app, here's a tip calculator!
+
+## What is this
+
+![Demo of calculating a tip for $51.33](demo.gif)
+
+This is an implementation of the Swift tip calculator built by following [tutorial video](http://vimeo.com/102084767) published by CodePath.
+
+Time spent: 1.5 hours
+
+Completed stories:
+
+ * [x] Required: Calculates tip value based on the user input
+ * [ ] Required: A settings button displays information about the app
+ * [ ] Optional: User can set a default tip value from settings
+
+## What's different
+
+- Converting text to double value is done using the NSString instead of Objective-C bridge.[reference](http://stackoverflow.com/questions/24031621/swift-how-to-convert-string-to-double)
+- Refactored the tip computation into a separate function and reusing it on change of both the amount and tip percentage.  
+
+## Credit
+- GIF created with [LiceCap](http://www.cockos.com/licecap/).
+- [@timothy1ee](https://github.com/timothy1ee) for the great [tutorial video](http://vimeo.com/102084767) 
+
+## License
+
+MIT
--- TipCalc/ViewController.swift
+++ TipCalc/ViewController.swift
@@ -10,16 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
+    @IBOutlet weak var tipPercentageSegControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func getTipPercentage() -> Double {
+        switch(tipPercentageSegControl.selectedSegmentIndex) {
+        case(0):
+            return 10.0
+        case(1):
+            return 15.0
+        case(2):
+            return 20.0
+        default:
+            return 0.0
+        }
+    }
+    
+    func refreshAmounts() {
+        println("refreshAmounts() called.")
+        var amount = (billField.text as NSString).doubleValue
+        println("Parsed amount is $\(amount)")
+        var tipPct = getTipPercentage()
+        println("Tip percentage: \(tipPct)")
+        var tip = (amount * tipPct)/100
+        var tipStr = String(format: "$%.2f", tip)
+        var total = amount + tip
+        var totalStr = String(format: "$%.2f", total)
+        println("Tip: $\(tipStr), Total: $\(totalStr)")
+        
+        tipLabel.text = tipStr
+        totalLabel.text = totalStr
+        
+    }
+    
+    @IBAction func onValueChanged(sender: AnyObject) {
+        refreshAmounts()
+    }
+    
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        refreshAmounts()
+    }
 
 }
 
